be93dd323fd1a3b31d44354c837612e28a86a18f
cesaragostino <90473152+cesaragostino@users.noreply.github.com>
2026-04-13 01:59:13 -0300

    SY.L1-L2 lookup

--- fluxbee-go-sdk/identity_other.go
+++
@@ -1,9 +0,0 @@
-//go:build !linux
-
-package sdk
-
-import "fmt"
-
-func openRouterSHMReadOnly(name string) ([]byte, func() error, error) {
-	return nil, func() error { return nil }, fmt.Errorf("router shm resolution is only supported on linux")
-}
